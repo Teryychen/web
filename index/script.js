34a112cd3ac9d0c4826adcf1f0af81592870528b
Teryychen <145099916+Teryychen@users.noreply.github.com>
2025-01-03 01:36:30 +0000

    a

---
+++ index/script.js
@@ -0,0 +1,17 @@
+// Splash Screen
+window.addEventListener('load', () => {
+    const splash = document.getElementById('splash-screen');
+    setTimeout(() => {
+        splash.style.display = 'none';
+    }, 2000);
+});
+
+// Add click event to project images
+document.querySelectorAll('.project-image').forEach(image => {
+    image.addEventListener('click', function () {
+        const modalImage = document.getElementById('modalImage');
+        modalImage.src = this.src; // Set the clicked image source to modal image
+        const modal = new bootstrap.Modal(document.getElementById('imageModal'));
+        modal.show();
+    });
+});
---
+++ sever.js
@@ -0,0 +1,17 @@
+//install: node js
+//install web server package: express >npm install express
+var express = require("express");
+var server = express();
+var bodyParser = require("body-parser");
+
+//web root
+server.use(express.static(__dirname+"/AgencyProject"));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded());
+
+var fileUpload = require("express-fileupload");
+server.use(fileUpload({defCharset:'utf8', defParamCharset:'utf8'}));
+
+server.listen(80, ()=>{
+    console.log("Server is running at port 80.");
+})
